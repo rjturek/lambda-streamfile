e1a8aa5e73c7b02c66e9eaf7a1ffac9c82fce7c9
Richard Turek <rjt1357@gmail.com>
2016-02-08 11:40:57 -0500

    porcess s3 event

--- streamfile.py
+++ streamfile.py
@@ -5,8 +5,10 @@ import json
 print('Loading function')
 
 def lambda_handler(event, context):
+    print('BBBBBBBBBBBBBBBBBBBBBB')
     print("Received event: " + json.dumps(event, indent=2))
-    print("File uploaded = " + event['Records'][0].s3.key + " from git")
+    print("File uploaded = " + event['Records'][0]['s3']['object']['key'] + " from git")
+    print('EEEEEEEEEEEEEEEEEEEEEE')
     return "hey there"
     #raise Exception('Something went wrong')
 
