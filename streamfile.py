354fc319a15b1d21eaf24486f94f14bc370b049c
Richard Turek <rjt1357@gmail.com>
2016-02-08 10:10:16 -0500

    initial

---
+++ streamfile.py
@@ -0,0 +1,13 @@
+from __future__ import print_function
+
+import json
+
+print('Loading function')
+
+def lambda_handler(event, context):
+    print("Received event: " + json.dumps(event, indent=2))
+    # print("value1 = " + event['key1'] + " from git")
+    # print("value2 = " + event['key2'] + " waddle doodle")
+    # print("value3 = " + event['key3'] + " from git")
+    return "hey there"
+    #raise Exception('Something went wrong')
