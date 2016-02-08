b89be22f714cae72aabbcea610c945c4bebcba5e
Richard Turek <rjt1357@gmail.com>
2016-02-08 12:11:53 -0500

    get file contents

--- streamfile.py
+++ streamfile.py
@@ -1,11 +1,14 @@
 from __future__ import print_function
+from boto.s3.connection import S3Connection
 
 import json
 
-print('Loading function')
+print('............. Loading function ...............')
+
+conn = S3Connection('AKIAJVIDFQIDLODBK5IA', 'lKQ7WokY7+RKU1cHAZsvywRHtwibbg0kJ+OcsFVp')
 
 def lambda_handler(event, context):
-    print('BBBBBBBBBBBBBBBBBBBBBB')
+    print('bbbbbbbbbbbbbbbbbbbbbb')
     print("Received event: " + json.dumps(event, indent=2))
     print("File uploaded = " + event['Records'][0]['s3']['object']['key'] + " from git")
     print('EEEEEEEEEEEEEEEEEEEEEE')
