a6e19606082dd6fde7ae6474a48bae275dd8be88
Richard Turek <rjt1357@gmail.com>
2016-02-08 13:04:22 -0500

    boto3

--- streamfile.py
+++ streamfile.py
@@ -1,18 +1,29 @@
 from __future__ import print_function
-from boto.s3.connection import S3Connection
+
+import boto3
+import botocore
 
 import json
 
 print('............. Loading function ...............')
 
-conn = S3Connection('AKIAJVIDFQIDLODBK5IA', 'lKQ7WokY7+RKU1cHAZsvywRHtwibbg0kJ+OcsFVp')
+
+#conn = S3Connection('AKIAJVIDFQIDLODBK5IA', 'lKQ7WokY7+RKU1cHAZsvywRHtwibbg0kJ+OcsFVp')
 
 def lambda_handler(event, context):
     print('bbbbbbbbbbbbbbbbbbbbbb')
     print("Received event: " + json.dumps(event, indent=2))
-    print("File uploaded = " + event['Records'][0]['s3']['object']['key'] + " from git")
+    print("File uploaded = " + event['Records'][0]['s3']['object']['key'] + " to S3")
     print('EEEEEEEEEEEEEEEEEEEEEE')
-    return "hey there"
+
+    s3 = boto3.resource('s3')
+    # bucket = s3.Bucket('skynetdatafiles')
+
+    for bucket in s3.buckets.all():
+        for key in bucket.objects.all():
+            print(key.key)
+
+    return "hey there done"
     #raise Exception('Something went wrong')
 
 ##
